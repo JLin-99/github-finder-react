5b3189cf7dd0a4adfcae58ed90671a3337d66ef8
José Lin <lin-jose@outlook.com>
2022-08-03 21:07:43 -0200

    Create Pages and Routes

---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+export default function About() {
+  return (
+    <div>
+      <h1 className="text-6xl mb-4">About</h1>
+      <p className="mb-4 text-2xl font-light">
+        A React App to search GitHub profiles and see profile details.
+      </p>
+      <p className="text-lg text-gray-400">Version 1.0.0</p>
+    </div>
+  );
+}
