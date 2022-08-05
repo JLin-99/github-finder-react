0654a9bc9d11a4723bc4b1e6a02373204cb34c59
José Lin <lin-jose@outlook.com>
2022-08-05 15:30:15 -0200

    Update Actions with Axios

--- src/context/github/gitHubActions.js
+++ src/context/github/gitHubActions.js
@@ -1,34 +1,56 @@
+import axios from "axios";
 const GITHUB_API_URL = process.env.REACT_APP_GITHUB_URL;
+
+const github = axios.create({
+  baseURL: GITHUB_API_URL,
+  headers: {}, // Can add Authorization token if want to
+});
 
 export const searchUsers = async (username) => {
   const params = new URLSearchParams({
     q: username,
   });
 
-  const response = await fetch(`${GITHUB_API_URL}/search/users?${params}`);
-  const { items } = await response.json();
+  // const response = await fetch(`${GITHUB_API_URL}/search/users?${params}`);
+  // const { items } = await response.json();
+  const response = await github.get(`/search/users?${params}`);
 
-  return items;
+  return response.data.items;
 };
 
-export const getUser = async (login) => {
+export const getUserWithFetch = async (login) => {
   const params = new URLSearchParams({
     sort: "created",
     per_page: 10,
   });
 
-  const response = await fetch(`${GITHUB_API_URL}/users/${login}`);
+  const [user, user_repos] = await Promise.all([
+    fetch(`${GITHUB_API_URL}/users/${login}`),
+    fetch(`${GITHUB_API_URL}/users/${login}/repos?${params}`),
+  ]);
 
-  if (response.status === 404) {
+  if (user.status === 404) {
     window.location = "/notfound";
   } else {
-    const data = await response.json();
-    const repos_response = await fetch(
-      `${GITHUB_API_URL}/users/${login}/repos?${params}`
-    );
-    const repos = await repos_response.json();
-    data.repos = repos;
+    const data = await user.json();
+    data.repos = await user_repos.json();
 
     return data;
   }
+};
+
+export const getUser = async (username) => {
+  const params = new URLSearchParams({
+    sort: "created",
+    per_page: 10,
+  });
+
+  const [user, repos] = await Promise.all([
+    github.get(`/users/${username}`),
+    github.get(`/users/${username}/repos?${params}`),
+  ]);
+
+  user.data.repos = repos.data;
+
+  return user.data;
 };
